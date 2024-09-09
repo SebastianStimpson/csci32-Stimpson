15db327bc70250a0724356bb10c267b110052729
SebastianStimpson <stimpsonsebastian094@gmail.com>
2024-09-09 16:22:33 -0700

    added the same photo to both pages

--- csci32-nextjs-stimpson/src/app/about/page.tsx
+++ csci32-nextjs-stimpson/src/app/about/page.tsx
@@ -28,7 +28,7 @@ export default function Portfolio() {
           </div>
           <div>
             <Image
-              src=""
+              src="https://rdcnewscdn.realtor.com/wp-content/uploads/2015/08/house-painter.jpg"
               alt="Painter's Work"
               width={500}
               height={500}
--- csci32-nextjs-stimpson/src/app/page.tsx
+++ csci32-nextjs-stimpson/src/app/page.tsx
@@ -25,7 +25,7 @@ export default function Home() {
           </div>
           <div>
             <Image
-              src=""
+              src="https://rdcnewscdn.realtor.com/wp-content/uploads/2015/08/house-painter.jpg"
               alt="Painter's Work"
               width={500}
               height={500}
