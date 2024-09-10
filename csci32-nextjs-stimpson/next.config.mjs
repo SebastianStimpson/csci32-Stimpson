541743057041631ebfac8ca1974a147eebcd424e
erikmellum <mellum.erik@gmail.com>
2024-09-10 14:35:40 -0700

    Fix again

--- csci32-nextjs-stimpson/next.config.mjs
+++ csci32-nextjs-stimpson/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['realtor.com', 'rdcnewscdn.realtor.com'],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
