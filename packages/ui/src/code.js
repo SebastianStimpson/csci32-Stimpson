ba5ef793395e18c4011a0ae01b7874d35527f57b
SebastianStimpson <stimpsonsebastian094@gmail.com>
2024-09-29 20:41:17 -0700

    found more .js files that where added

--- packages/ui/src/code.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Code = Code;
-function Code(_a) {
-    var children = _a.children, className = _a.className;
-    return <code className={className}>{children}</code>;
-}
--- packages/ui/src/tokens.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCommonStyles = getCommonStyles;
-function getCommonStyles() {
-    return 'flex items-center justify-center focus:outline outline-offset-2 transition-colors';
-}
