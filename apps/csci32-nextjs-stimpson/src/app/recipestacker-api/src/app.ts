60b0d2d2a5fd36aa57ef1159dc62c44f686c382c
SebastianStimpson <stimpsonsebastian094@gmail.com>
2024-10-14 11:43:33 -0700

    added the missing files in plugins

--- apps/csci32-nextjs-stimpson/src/app/recipestacker-api/src/app.ts
+++ apps/csci32-nextjs-stimpson/src/app/recipestacker-api/src/app.ts
@@ -1,28 +1,18 @@
 import { FastifyInstance } from 'fastify'
 import sensible from '@fastify/sensible'
-import cors from '@fastify/cors'
-import swagger from '@fastify/swagger'
-import swaggerUI from '@fastify/swagger-ui'
-import rootRoutes from './routes/root'
-import recipeRoutes from './routes/recipes'
+import cors from './plugins/cors'
+import swagger from './plugins/swagger'
 import prismaPlugin from './plugins/prisma'
 import recipePlugin from './plugins/recipe'
+import rootRoutes from './routes/root'
+import recipeRoutes from './routes/recipes'
 
 export default async function app(fastify: FastifyInstance) {
   fastify.register(sensible)
-  fastify.register(cors, { origin: '*' })
 
-  fastify.register(swagger, {
-    swagger: {
-      info: {
-        title: 'RecipeStacker API',
-        description: 'API documentation for RecipeStacker',
-        version: '1.0.0',
-      },
-    },
-  })
+  fastify.register(cors)
 
-  fastify.register(swaggerUI, { routePrefix: '/docs' })
+  fastify.register(swagger)
 
   fastify.register(prismaPlugin)
   fastify.register(recipePlugin)
